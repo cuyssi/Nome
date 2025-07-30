45e8afcb85f89f7bfbe82c9be3d36ee0075c7fa0
cuyssi <anabcastrolopez@gmail.com>
2025-07-30 20:19:42 +0200

    fix: funtion delete task

--- frontend/src/components/task/Tasks_list.jsx
+++ frontend/src/components/task/Tasks_list.jsx
@@ -11,13 +11,14 @@ import { Task_card } from "./Task_card";
 import { useTasks } from "../../hooks/useTasks";
 
 export const Tasks_list = ({ tasks = [], openModalWithTask, className = "" }) => {
+    const { deleteTask } = useTasks();
   return (
     <div className={`flex flex-col gap-8 items-center w-full h-[74%] overflow-y-auto px-2 py-4 ${className}`}>
       {tasks.map((task) => (
         <Task_card
           key={task.id}
           task={task}
-          onDelete={() => {}}
+          onDelete={() => deleteTask(task.id)}
           onEdit={() => openModalWithTask(task)}
         />
       ))}
