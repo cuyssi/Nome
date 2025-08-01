e0bf40e162a2e7e710effdd57c8f3e5a0106a81d
cuyssi <anabcastrolopez@gmail.com>
2025-08-01 11:04:33 +0200

    fix: edition in Today_tasks

--- frontend/src/pages/Today_tasks.jsx
+++ frontend/src/pages/Today_tasks.jsx
@@ -1,9 +1,41 @@
 import { Tasks_list } from "../components/task/Tasks_list";
 import { useTasks } from "../hooks/useTasks";
+import { useState } from "react";
+import { useStorageStore } from "../store/storageStore";
+import { Modal } from "../components/commons/Modal";
+import { Form } from "../components/commons/Form";
+import { useModalStore } from "../store/modalStore";
 
-const Today_tasks = () => {
+
+const Today_tasks = ({ type, exclude = false }) => {
   const { todayTasks } = useTasks();
-  console.log("todayTasks in today:", todayTasks);
+  const [activeTab, setActiveTab] = useState("citas");
+    const { reload } = useTasks(type, exclude);
+    const [renderKey, setRenderKey] = useState(0);
+    const [showModalConfirmation, setShowModalConfirmation] = useState(false);
+    const { isOpen, selectedTask, openModalWithTask, closeModal } = useModalStore();
+    const { updateTask } = useStorageStore();
+    const { tasks } = useTasks(type, exclude);
+
+    const handleCloseModal = () => {
+    setRenderKey((prev) => prev + 1);
+    closeModal();
+};
+
+    const handleEditTask = (updatedTask) => {
+        updateTask(updatedTask);
+        reload();
+        setRenderKey((prev) => prev + 1);
+        setShowModalConfirmation(true);
+
+        setTimeout(() => {
+            setShowModalConfirmation(false);
+            closeModal();
+        }, 1500);
+    };
+
+    const tiposExcluir = ["medico", "deberes", "trabajo"];
+
 
   return (
     <div className="flex flex-col h-[100%] items-center overflow-hidden">
@@ -11,8 +43,21 @@ const Today_tasks = () => {
                 Tareas para Hoy
             </h2>
             <div className="w-[90%] h-[90%]">
-                <Tasks_list tasks={todayTasks} />
+                <Tasks_list tasks={todayTasks} openModalWithTask={openModalWithTask} />
             </div>
+            {isOpen ? (
+                <Modal onClose={handleCloseModal}>
+                    {showModalConfirmation ? (
+                        <p className="text-green-500 text-center font-semibold animate-fadeIn">
+                            ✅ Cambios guardados con éxito
+                        </p>
+                    ) : selectedTask && selectedTask.id ? (
+                        <Form task={selectedTask} onSubmit={handleEditTask} onClose={handleCloseModal} />
+                    ) : (
+                        <p className="text-yellow-300 text-center">Cargando tarea seleccionada…</p>
+                    )}
+                </Modal>
+            ) : null}
         </div>
     );
 };
